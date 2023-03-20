eb377df8338ead8a7cd0a3d9a362b5074744ed23
Yasmine Reis <yreis@wisc.edu>
2023-03-20 09:17:27 -0500

    removed cars priced less than 500

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+
 import pandas as pd
 import numpy as np
 import csv
+import matplotlib.pyplot as plt
+
 
 #read in original dataframe
 df =  pd.read_csv(r'autos.csv')
@@ -10,7 +13,8 @@ df = df[['price','vehicleType','yearOfRegistration','gearbox','powerPS','model',
         'kilometer','fuelType','brand','notRepairedDamage','dateCreated','postalCode']]
 
 #cleaning data, removing incomplete rows
-df = df[df['price']!=0] #removing rows where price is 0
+df = df[df['price']>500] #removing rows where price less than $10
+df = df[df['price']<100000] #removing price outliers over $100k
 df = df[df['powerPS']!=0] #removing rows where horsepower is 0
 df = df.dropna() #removing all NaN values
 
@@ -31,14 +35,16 @@ df = df[df.brand.isin(lst) == False]
 lst2=['andere','bus']
 df = df[df.vehicleType.isin(lst2) == False]
 
-#should now be at 192,472 rows
-print(df)
+#should now be at 184,096 rows
+#print(df)
 
 #CLEAN, READY TO VISUALIZE
 
+#scatterplot of price vs year of registration
+#df.plot.scatter(x = "price", y = "yearOfRegistration")
+#plt.show()
 
-
-
-
+#df.plot.hist(by = "powerPS")
+#plt.show()
 
 
